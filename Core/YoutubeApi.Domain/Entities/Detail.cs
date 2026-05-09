b2937b845cf72d3721103767bceb7eebb2eeec9f
Sıla Dertli <siladertli39@gmail.com>
2026-05-09 19:23:21 +0300

    temel entityler oluşturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+using YoutubeApi.Domain.Entities;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail:EntityBase
+    {
+        public Detail()
+        {
+                
+        }
+        public Detail(string title, string description, int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public required string Title  { get; set; }
+        public required string Description { get; set; }
+
+        // Bir detail bir Category'e bağlı
+        public required int CategoryId { get; set; }
+        public required Category Category { get; set; }
+
+    }
+
+}
---
+++ Core/YoutubeApi.Domain/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Product: EntityBase
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; }  
+        public required Brand BrandId { get; set; }
+        public Brand Brand { get; set; }
+
+        // Bir kategorinin birden fazla ürünü olabilir, otomatik olarak oluşturulan bir ilişki tablosu ile ilişkilendirilir
+        public ICollection<Category> Categories { get; set; }
+
+        // public required string ImagePath { get; set; }   
+    }
+}
